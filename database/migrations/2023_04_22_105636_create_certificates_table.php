e70f8532385fcd3d7486b06b2e9b14d15d0b549b
i-bm <isaac.boakyemanu@gmail.com>
2023-04-22 11:13:16 +0000

    initial commit

---
+++ database/migrations/2023_04_22_105636_create_certificates_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('certificates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('type');
+            $table->string('unique_code');
+            $table->string('hash');
+            $table->boolean('isActive')->default(1)->comment('0 - Inactive , 1 - Active');
+            $table->integer('link_click_count')->comment('The number of times link has been clicked.');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('certificates');
+    }
+};
